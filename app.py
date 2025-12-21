848e54988edffa7ea56c769d79cd3a90939dd5d3
Fanar Rofoo <fanarrofoo@gmail.com>
2025-12-21 19:41:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ if st.session_state.prediction is not None:
     st.info(f"Class ID: {st.session_state.prediction}")
     
 if st.session_state.prediction >= 4: # st.ballons()
-st.success("The operation was completed successfully.")
+    st.success("The operation was completed successfully.")
+    
     # --- 5. Feedback Section ---
 if st.session_state.prediction is not None:
     st.divider()
