501b8a22fb73098ff069b745f96e986407258b2b
Fanar Rofoo <fanarrofoo@gmail.com>
2025-12-21 19:46:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if st.session_state.prediction is not None:
     st.subheader(f"Predicted Sentiment: **{st.session_state.label}**")
     st.info(f"Class ID: {st.session_state.prediction}")
     
-if st.session_state.prediction >= 0:
+if st.session_state.prediction >=0:
     st.success("The operation was completed successfully.")
     
  # --- 5. Feedback Section ---
